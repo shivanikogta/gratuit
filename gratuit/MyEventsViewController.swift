c896cbbaeca6df5044dedda1ada01b3bb77804d9
sayamkanwar <sayamkanwar616@gmail.com>
2022-03-28 17:03:17 -0400

    init commit
    Created 5 UI View Controllers and linked them to the respective view
    controller source codes

---
+++ gratuit/MyEventsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyEventsViewController.swift
+//  gratuit
+//
+//  Created by Sayam Kanwar on 28/03/22.
+//
+
+import UIKit
+
+class MyEventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
