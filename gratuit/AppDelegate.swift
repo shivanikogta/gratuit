e084fbec1f3f83a7f78d4e9a7e59152691486678
SohumT <70512473+SohumT@users.noreply.github.com>
2022-04-04 17:30:41 -0400

    Commit Map methods

--- gratuit/AppDelegate.swift
+++ gratuit/AppDelegate.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import GoogleMaps
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        GMSServices.provideAPIKey("AIzaSyBluXbCjtGQuJzAd-KZ-BVfzzWQPTiNI3A")
         return true
     }
 
--- gratuit/MapViewController.swift
+++ gratuit/MapViewController.swift
@@ -6,15 +6,50 @@
 //
 
 import UIKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Create a GMSCameraPosition that tells the map to display the
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        
+        var locationManager = CLLocationManager()
+        lazy var mapView = GMSMapView()
+        mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
+        self.view.addSubview(mapView)
+        
+        // Find current location of user
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+
+        /* Creates a marker - use with for loop to set event markers.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "User"
+        marker.map = mapView */
 
         // Do any additional setup after loading the view.
     }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let userLocation = locations.last
+        let center = CLLocationCoordinate2D(latitude: userLocation!.coordinate.latitude, longitude: userLocation!.coordinate.longitude)
+
+        let camera = GMSCameraPosition.camera(withLatitude: userLocation!.coordinate.latitude,
+                                                              longitude: userLocation!.coordinate.longitude, zoom: 13.0)
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        self.view = mapView
+
+        locationManager.stopUpdatingLocation()
+    }
+
 
     /*
     // MARK: - Navigation
