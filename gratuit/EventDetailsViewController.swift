95101d9ac619f9f0c28a88145f12ec20df6e89c6
SohumT <70512473+SohumT@users.noreply.github.com>
2022-04-15 14:34:37 -0400

    Commit with adding markers (test 1)

---
+++ gratuit/EventDetailsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EventDetailsViewController.swift
+//  gratuit
+//
+//  Created by Sohum Thadani on 4/14/22.
+//
+
+import UIKit
+import Parse
+
+class EventDetailsViewController: UIViewController {
+    
+    var event = PFObject()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- gratuit/MapViewController.swift
+++ gratuit/MapViewController.swift
@@ -14,6 +14,8 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
     
     @IBOutlet weak var mapView: GMSMapView!
     
+    var events = [PFObject]()
+    
     let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
@@ -28,6 +30,40 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
         mapView.delegate = self
 
         // Do any additional setup after loading the view.
+        mapView.settings.myLocationButton = true
+        mapView.isMyLocationEnabled = true
+        
+        mapView.settings.scrollGestures = true
+        mapView.settings.zoomGestures = true
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className: "Events")
+        query.includeKeys(["eventID", "eventName", "eventDescription", "eventLocation", "startTime", "endTime", "creator"])
+
+    
+        query.findObjectsInBackground { (events, error) in
+            if events != nil {
+                self.events = events!
+            }
+        }
+        
+        for event in events {
+            
+            // replace position later with variables
+            
+            let position = CLLocationCoordinate2D(latitude: 10, longitude: 10)
+            let marker = GMSMarker(position: position)
+            marker.title = event["eventName"] as! String
+            marker.map = mapView
+            
+        }
+        
+        
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -37,10 +73,10 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
         let location = locations.last
         
         
-        // Set Marker Position of User - Later include for loop for adding all the markers from PFObject here
+        /* Set Marker Position of User - Later include for loop for adding all the markers from PFObject here
         let marker = GMSMarker(position: location!.coordinate)
         marker.title = "User Location"
-        marker.map = mapView
+        marker.map = mapView */
         
         let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!,
                                                       longitude: (location?.coordinate.longitude)!,
@@ -73,5 +109,33 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, GMSMapView
         UserDefaults.standard.set(false, forKey: "userLoggedIn")
         delegate.window?.rootViewController = LoginViewController
     }
+    
+    func mapView(mapView: GMSMapView!, didTapMarker marker: GMSMarker!) -> Bool {
+        
+        print("tapped")
+        
+        for event in events {
+            
+            // replace with position variables later
+            
+            let location = CLLocationCoordinate2D(latitude: 10, longitude: 10)
+            
+            if (location.latitude == marker.position.latitude
+                && location.longitude == marker.position.longitude) {
+                
+                // let sender: PFObject = event
+                
+                // performSegue(withIdentifier: "goToCreateEvents", sender: sender)
+                
+                break
+            }
+            
+        }
+        
+        
+        return true
+    }
+    
+    
 
 }
