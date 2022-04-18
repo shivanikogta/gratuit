9a8df55de47bd820d90a3d41cf271a06aa86bdf9
Rohan Purandare <rpuranda@purdue.edu>
2022-04-18 16:21:29 -0400

    potentially finished the basic version of event list view

---
+++ gratuit/EventCell.swift
@@ -0,0 +1,27 @@
+//
+//  EventCell.swift
+//  gratuit
+//
+//  Created by Rohan Purandare on 4/18/22.
+//
+
+import UIKit
+
+class EventCell: UITableViewCell {
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var eventName: UILabel!
+    @IBOutlet weak var eventLocation: UILabel!
+    @IBOutlet weak var eventTime: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- gratuit/EventsListViewController.swift
+++ gratuit/EventsListViewController.swift
@@ -6,16 +6,59 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class EventsListViewController: UIViewController {
+class EventsListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var events = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let query = PFQuery(className: "Event")
+        query.limit = 20
+        
+        query.findObjectsInBackground{(events, error) in
+            if events != nil {
+                self.events = events!
+                self.tableView.reloadData()
+            }
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell") as! EventCell
+        let event = events[indexPath.row]
+        cell.eventName.text = event["eventName"] as! String
+        cell.eventTime.text = event["startTime"] as! String
+        cell.eventLocation.text = event["eventLocation"] as! String
+        
+        let imageFile = event["eventImage"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af.setImage(withURL: url)
+        
+        return cell
+    }
+    
+    
     /*
     // MARK: - Navigation
 
