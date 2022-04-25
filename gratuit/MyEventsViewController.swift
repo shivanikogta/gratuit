4c85969819a87c773a12a654e932dac4e25b4d2e
Sayam Kanwar <skanwar@purdue.edu>
2022-04-25 17:09:50 -0400

    Last minute fixes

--- gratuit/MyEventsViewController.swift
+++ gratuit/MyEventsViewController.swift
@@ -42,35 +42,36 @@ class MyEventsViewController: UIViewController, UITableViewDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return events.count
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let event = events[indexPath.row]
-        let isStarred = event["starred"] as! Bool
+//        let event = events[indexPath.row]
+//        let isStarred = event["starred"] as! Bool
+//        var counter = 0
         let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell") as! EventCell
-        var counter = 0
-        if (isStarred) {
-            //let event = events[indexPath.row]
-            let dateTime = event["startTime"] as! String
-            let timeComponents = dateTime.components(separatedBy: ", ")
-            let date = timeComponents[0]
-            let startTime = timeComponents[1]
-            let endDateTime = event["endTime"] as! String
-            let endTimeComponents = endDateTime.components(separatedBy: ", ")
-            let endTime = endTimeComponents[1]
-            let time = startTime + " - " + endTime
-            cell.eventName.text = event["eventName"] as! String
-            cell.eventTime.text = time
-            cell.eventLocation.text = event["eventLocation"] as! String
-            cell.date.text = date
-            let imageFile = event["eventImage"] as! PFFileObject
-            let urlString = imageFile.url!
-            let url = URL(string: urlString)!
-            
-            cell.photoView.af.setImage(withURL: url)
-        }
+//        if (isStarred) {
+//            //let event = events[indexPath.row]
+//            let dateTime = event["startTime"] as! String
+//            let timeComponents = dateTime.components(separatedBy: ", ")
+//            let date = timeComponents[0]
+//            let startTime = timeComponents[1]
+//            let endDateTime = event["endTime"] as! String
+//            let endTimeComponents = endDateTime.components(separatedBy: ", ")
+//            let endTime = endTimeComponents[1]
+//            let time = startTime + " - " + endTime
+//            cell.eventName.text = event["eventName"] as! String
+//            cell.eventTime.text = time
+//            cell.eventLocation.text = event["eventLocation"] as! String
+//            cell.date.text = date
+//            let imageFile = event["eventImage"] as! PFFileObject
+//            let urlString = imageFile.url!
+//            let url = URL(string: urlString)!
+//
+//            cell.photoView.af.setImage(withURL: url)
+//        }
         return cell
+//        counter += 1
     }
     /*
     // MARK: - Navigation
